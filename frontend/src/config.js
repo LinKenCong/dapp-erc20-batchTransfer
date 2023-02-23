8ca324fa761d52b31dbaf1bb4657ff4af846cb82
kyrielam <lamkencong@gmail.com>
2023-02-24 01:40:21 +0800

    add website set config

---
+++ frontend/src/config.js
@@ -0,0 +1,14 @@
+export const Contract_BatchTransfer = '0x'
+export const Chain_Params = [
+  {
+    chainId: '0x38',
+    chainName: 'BNB Smart Chain Mainnet',
+    rpcUrls: ['https://bsc-dataseed1.binance.org/'],
+  },
+  {
+    chainId: '0x61',
+    chainName: 'BNB Smart Chain Testnet',
+    rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
+  },
+]
+export const Net_Id = 0
