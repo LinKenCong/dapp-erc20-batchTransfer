18aec0a564f4c9bcd8a457e338219a21465757f0
kyrielam <lamkencong@gmail.com>
2023-02-25 01:45:51 +0800

    add testnet set

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const Contract_BatchTransfer = '0x'
+export const Contract_BatchTransfer = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
 export const Chain_Params = [
   {
     chainId: '0x38',
@@ -10,5 +10,10 @@ export const Chain_Params = [
     chainName: 'BNB Smart Chain Testnet',
     rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545/'],
   },
+  {
+    chainId: '0x7a69',
+    chainName: 'Local Testnet',
+    rpcUrls: ['http://localhost:8545/'],
+  },
 ]
-export const Net_Id = 0
+export const Net_Id = 2
