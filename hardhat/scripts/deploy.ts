f2f90aadc341368d80592f23df0148260ad72597
kyrielam <lamkencong@gmail.com>
2023-02-25 01:46:59 +0800

    add contract deploy

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,23 +1,21 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const BatchTransferFactory = await ethers.getContractFactory('BatchTransfer')
+  const TestCoinFactory = await ethers.getContractFactory('TestCoin')
+  const batchTransfer = await BatchTransferFactory.deploy()
+  const testCoin = await TestCoinFactory.deploy()
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await batchTransfer.deployed()
+  await testCoin.deployed()
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  console.log(`batchTransfer contract deployed to ${batchTransfer.address}`)
+  console.log(`testCoin contract deployed to ${testCoin.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
