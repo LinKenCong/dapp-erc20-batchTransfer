492a4dc0d1fef0fff1dabccbb754f979a51d2f18
kyrielam <lamkencong@gmail.com>
2023-02-28 21:35:35 +0800

    del fee

--- hardhat/contracts/BatchTransfer.sol
+++ hardhat/contracts/BatchTransfer.sol
@@ -4,10 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BatchTransfer is Ownable {
-    // using SafeERC20 for IERC20;
-    uint256 public fee;
 
-    function batchTransfer(address _token, address[] calldata _tos, uint256 _amount) external payable payFee {
+    function batchTransfer(address _token, address[] calldata _tos, uint256 _amount) external{
         IERC20 token = IERC20(_token);
         require(token.allowance(msg.sender, address(this)) >= _tos.length * _amount, "Need approve ERC20 token");
         for (uint8 i = 0; i < _tos.length; i++) {
@@ -15,7 +13,7 @@ contract BatchTransfer is Ownable {
         }
     }
 
-    function batchCall(address _token, address[] calldata _tos, uint256 _amount) external payable payFee {
+    function batchCall(address _token, address[] calldata _tos, uint256 _amount) external{
         IERC20(_token).transferFrom(msg.sender, address(this), _tos.length * _amount);
         for (uint8 i = 0; i < _tos.length; i++) {
             bytes memory callData = abi.encodeWithSignature("transfer(address,uint256)", _tos[i], _amount);
@@ -23,23 +21,4 @@ contract BatchTransfer is Ownable {
             require(success, "BatchTransfer: call failed");
         }
     }
-
-    function updateFee(uint256 _newFee) external onlyOwner {
-        fee = _newFee;
-    }
-
-    function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-        (bool success, ) = payable(owner()).call{value: balance}("");
-        if (!success) revert("withdraw error");
-    }
-
-    modifier payFee() {
-        require(msg.value >= fee, "Need pay fee");
-        _;
-    }
-
-    receive() external payable {}
-
-    fallback() external payable {}
 }
